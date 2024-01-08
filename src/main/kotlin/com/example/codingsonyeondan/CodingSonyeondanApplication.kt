fd78433ca7f52c6e0d75e5d293b512adc15844a9
tlsgkdns <tlsgkdns@gmail.com>
2024-01-08 15:25:07 +0900

    first Commit

---
+++ src/main/kotlin/com/example/codingsonyeondan/CodingSonyeondanApplication.kt
@@ -0,0 +1,11 @@
+package com.example.codingsonyeondan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CodingSonyeondanApplication
+
+fun main(args: Array<String>) {
+    runApplication<CodingSonyeondanApplication>(*args)
+}
